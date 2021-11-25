2ccdfc59ad5a239815fbf67a272825bb6c98aa94
卡拉马佐夫 <973459080@qq.com>
2021-11-25 14:51:37 +0800

    AuthRecordManager update

--- contracts/AuthRecordManager.sol
+++ contracts/AuthRecordManager.sol
@@ -17,10 +17,10 @@ contract AuthRecordManager {
         address userId;
         //数据目录id
         string schemaId;
-        // [1]: authState; [2]: transferState;
+        // [0]: authState; [1]: transferState;
         // authState：0-初始化，1-组织方已认证, 2-提供方已认证, 3-撤销
         // transferState：0-待发送，1-已发送，2-已接收 3-接收方数据校验错误
-        uint8[2] _intArray;
+        uint8[2] intArray;
         //数据指纹信息
         string dataHash;
         //用途ID
@@ -39,48 +39,48 @@ contract AuthRecordManager {
     function createAuthRecord(address receiverId, address providerId, address userId,string schemaId, string purposeId) public returns(uint256) {
         uint256 authRecordId = _authRecordCount;
         _authRecordCount = _authRecordCount + 1;
-        _authRecordMap[authRecordId] = _authRecordMap(authRecordId, receiverId, providerId, userId, schemaId, [1,0],"", purposeId,[now,0,0]);
+        _authRecordMap[authRecordId] = AuthRecord(authRecordId, receiverId, providerId, userId, schemaId, [1,0],"", purposeId,[now,0,0]);
         return (authRecordId);
     }
 
     /**
      * 取消授权记录，修改授权状态为已取消
      */
-    function revoke(string authRecordId) public {
+    function revoke(uint256 authRecordId) public {
         AuthRecord storage authRecord = _authRecordMap[authRecordId];
         require(msg.sender == authRecord.userId, "AuthRecordManager : account must be userID");
-        authRecord.authState = 3;
+        authRecord.intArray[0] = 3;
         authRecord.timeArray[1] = now;
     }
 
     /**
      * 提供方授权完成，上链修改授权状态为完成态
      */
-    function changeAuthState(string authRecordId) public {
-        _authRecordMap[authRecordId].authState = 2;
+    function changeAuthState(uint256 authRecordId) public {
+        _authRecordMap[authRecordId].intArray[0] = 2;
     }
 
     /**
      * 提供方传输数据，上传指纹，并修改传输状态为已发送
      */
-    function uploadEvidence(string authRecordId, string dataHash) public  {
+    function uploadEvidence(uint256 authRecordId, string dataHash) public  {
         AuthRecord storage authRecord = _authRecordMap[authRecordId];
-        authRecord.transferState = 1;
+        authRecord.intArray[1] = 1;
         authRecord.timeArray[2] = now;
     }
 
     /**
      * 接收方校验数据,修改传输状态
      */
-    function changeTransferState(string authRecordId, uint8 transferState) public  {
-        _authRecordMap[authRecordId].transferState = transferState;
+    function changeTransferState(uint256 authRecordId, uint8 transferState) public  {
+        _authRecordMap[authRecordId].intArray[1] = transferState;
     }
 
     /**
      * 查询授权记录
      */
-    function getAuthRecordById(string authRecordId) public view returns(string, address, address, address, string, uint8[2], string, string,uint8[2]) {
+    function getAuthRecordById(uint256 authRecordId) public view returns(uint256, address, address, address, string, uint8[2], string, string,uint256[3]) {
          AuthRecord storage authRecord = _authRecordMap[authRecordId];
-         return (authRecord);
+         return (authRecord.authRecordId,authRecord.receiverId,authRecord.providerId,authRecord.userId,authRecord.schemaId,authRecord.intArray,authRecord.dataHash,authRecord.purposeId,authRecord.timeArray);
     }
 }
