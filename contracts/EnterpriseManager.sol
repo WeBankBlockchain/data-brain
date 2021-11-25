a625287f55e1040856455b4f9bf68c266f86a8b1
aoronchu <aoronchu@gmail.com>
2021-11-25 18:30:54 +0800

    .

--- contracts/EnterpriseManager.sol
+++ contracts/EnterpriseManager.sol
@@ -2,8 +2,18 @@ pragma solidity ^0.4.25;
 
 import "./BasicAuth.sol";
 
-
 contract EnterpriseManager is BasicAuth{
+
+    uint8 public constant STATUS_NOT_EXIST = 0;
+    uint8 public constant STATUS_REGISTERED = 1;
+    uint8 public constant STATUS_AUTHENTICATED = 2;
+    uint8 public constant STATUS_REVOKED = 3;
+
+    event RegisterEnterprise(string id, string name, string phone, string location, string email);
+    event AuthenticateEnterprise(string id, bytes32 certHash);
+    event ModifyEnterprise(string id, string name, string phone, string location, string email);
+    event RemoveEnterprise(string id);
+
     //企业信息
     struct EnterpriseInfo{
         string id;
@@ -17,12 +27,13 @@ contract EnterpriseManager is BasicAuth{
         string email;
         //状态：0-不存在 1-已注册 2-已认证 3-已销户
         uint8 status;
+        //认证数据哈希
+        bytes32 certHash;
         //时间戳 0-注册时间戳 1-认证时间戳 2-销户时间戳 3-更新时间戳
         uint256[] timestamps;
     }
 
     mapping(string=>Enterprise) private enterprises;
-    mapping(string=>CertificationInfo) private certifications;
 
     function getEnterpriseStatus(string id) public view returns(uint) {
         return enterprises[id].status;
@@ -30,21 +41,37 @@ contract EnterpriseManager is BasicAuth{
 
     //注册企业数据
     function registerEnterprise(string id, string name, string phone, string location, string email) public onlyOwner{
-
+        require(enterprises[id].status == STATUS_NOT_EXIST, "Enterprise already existed");
+        EnterpriseInfo storage enterprise = enterprises[id];
+        enterprise.name = name;
+        enterprise.phone = phone;
+        enterprise.location = location;
+        enterprise.email = email;
+        enterprise.status = STATUS_REGISTERED;
+        emit RegisterProduct(id, name, phone, location, email);
     }
 
     //认证
-    function approveEnterpriseCert(string id, bytes32 certHash) public onlyOwner {
-
+    function authenticateEnterpriseCert(string id, bytes32 certHash) public onlyOwner {
+        require(enterprises[id].status == STATUS_REGISTERED, "Enterprise not registered");
+        enterprise.certHash = certHash;
+        enterprise.status = STATUS_AUTHENTICATED;
+        emit AuthenticateEnterprise(id, certHash);
     }
 
     //修改企业信息
     function modifyEnterprise(string id, string name, string phone, string location, string email) public onlyOwner {
-
+        require(enterprises[id].status == STATUS_REGISTERED  || enterprises[id].status == STATUS_AUTHENTICATED , "Enterprise must register");
+        enterprise.certHash = certHash;
+        enterprise.status = STATUS_AUTHENTICATED;
+        emit ModifyEnterprise(id, name, phone, location, email);
     }
 
     //销户企业
     function removeEnterprise(string id) public onlyOwner {
-
+        require(enterprises[id].status == STATUS_REGISTERED  || enterprises[id].status == STATUS_AUTHENTICATED , "Enterprise must register");
+        enterprise.certHash = certHash;
+        enterprise.status = STATUS_AUTHENTICATED;
+        emit RemoveProduct(id);
     }
 }
--- contracts/ProductManager.sol
+++ contracts/ProductManager.sol
@@ -5,23 +5,41 @@ import "./EnterpriseManager.sol";
 
 contract ProductManager is BasicAuth {
 
-    struct Product {
+    event RegisterProduct(string externalAddress, string name);
+    event RemoveProduct(string externalAddress);
+
+    struct ProductInfo {
         //产品地址
         address externalAddress;
         //机构地址
         string ownerId;
         //产品名称
         string name;
+        uint8 status;
     }
 
-    mapping(address => Product) private products;
+    mapping(address => ProductInfo) private products;
 
     //注册产品
     function registerProduct(
         string externalAddress,
         string name
-    ) public onlyOwner {}
+    ) public onlyOwner {
+        require(products[externalAddress].status == 0, "already registered");
+        ProductInfo storage p = products[externalAddress];
+        p.externalAddress = externalAddress;
+        p.name = name;
+        p.status = 1;
+        emit RegisterProduct(externalAddress, name);
+    }
 
     //注销产品
-    function removeProduct(string productAddress) public onlyOwner {}
+    function removeProduct(string externalAddress) public onlyOwner {
+        require(products[externalAddress].status == 1, "Not registered");
+        ProductInfo storage p = products[externalAddress];
+        p.externalAddress = address(0);
+        p.name = "";
+        p.status = 0;
+        emit RemoveProduct(externalAddress);
+    }
 }
